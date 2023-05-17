12fab0caf9bb9d52a1ed596af391de682893ea02
kobi <kobi2910@gmail.com>
2023-05-17 07:24:25 +0300

    "seconde version"

--- wether.js
+++ wether.js
@@ -1,34 +1,28 @@
-function refreshWether() {
-    cityName = ["London", "Jerusalem", "New York"]
-    apiKey = "8e6e3c00698e07493402345ddf4c6167"
-    urlLondon = `https://api.openweathermap.org/data/2.5/weather?q=${cityName[0]}&appid=${apiKey}`
-    urlJerusalem = `https://api.openweathermap.org/data/2.5/weather?q=${cityName[1]}&appid=${apiKey}`
-    urlNewYork = `https://api.openweathermap.org/data/2.5/weather?q=${cityName[2]}&appid=${apiKey}`
-    fetch(urlLondon)
-    .then(Response => Response.json())
-    .then(Date => {
-        document.getElementById("London").innerText = (Date.main.temp - 273.15).toFixed(0)
-    })
-    fetch(urlJerusalem)
-    .then(Response => Response.json())
-    .then(Date => {
-        document.getElementById("JRU").innerText = (Date.main.temp - 273.15).toFixed(0)
-    })
-    fetch(urlNewYork)
-    .then(Response => Response.json())
-    .then(Date => {
-        document.getElementById("NYU").innerText = (Date.main.temp - 273.15).toFixed(0)
-    })
+const cityName = ["London", "Jerusalem", "New York"];
+const apiKey = "8e6e3c00698e07493402345ddf4c6167";
+
+function fetchWeatherAndUpdateElement(city, elementId) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((weatherData) => {
+      const temperature = (weatherData.main.temp - 273.15).toFixed(0);
+      document.getElementById(elementId).innerText = temperature;
+    });
 }
-function refreshWetherByCity() {
-    apiKey = "8e6e3c00698e07493402345ddf4c6167"
-    city = document.getElementById("citySelector").value
-    url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-    fetch(url)
-    .then(Response => Response.json())
-    .then(data => {
-        document.getElementById("CityTemp").innerText = (data.main.temp - 273.15).toFixed(0)
-    })
-    document.getElementById("City").innerText = city
+
+function refreshWeather() {
+  fetchWeatherAndUpdateElement(cityName[0], "London");
+  fetchWeatherAndUpdateElement(cityName[1], "JRU");
+  fetchWeatherAndUpdateElement(cityName[2], "NYU");
 }
-refreshIntrval = setInterval(refreshWether, 60*1000)
+
+function refreshWeatherByCity() {
+  const city = document.getElementById("citySelector").value;
+  fetchWeatherAndUpdateElement(city, "CityTemp");
+  document.getElementById("City").innerText = city;
+}
+
+refreshWeather()
+const refreshInterval = setInterval(refreshWeather, 60 * 1000);
